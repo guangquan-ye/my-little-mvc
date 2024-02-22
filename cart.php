e8688ba2294d4a440681338fd5fa97cab505c94d
jeremy <jeremy.nowak@laplateforme.io>
2024-02-22 18:25:43 +0100

    cart.php Creation

---
+++ cart.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'vendor/autoload.php';
+if (isset($_SESSION)) {
+    session_start();
+}
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image" href="cart-icon.png">
+    <title>Cart</title>
+</head>
+<body>
+    
+</body>
+</html>
